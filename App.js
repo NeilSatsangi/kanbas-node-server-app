c891f9cbba526a85126bd056da4c8d588f48d1f8
neilsatsangi <neil.satsangi24@gmail.com>
2024-04-17 13:22:37 -0400

    trying stuff

--- App.js
+++ App.js
@@ -9,20 +9,21 @@ import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 
-
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
+
 app.use(cors({
     credentials: true,
-    origin: "https://kanbas-node-server-app-a6-91qt.onrender.com",
-
+    origin: process.env.FRONTEND_URL,
 }));
+
 const sessionOptions = {
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-  };
+};
+
 if (process.env.NODE_ENV !== "development") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
@@ -31,16 +32,9 @@ if (process.env.NODE_ENV !== "development") {
         domain: process.env.HTTP_SERVER_DOMAIN,
     };
 }
+
 app.use(session(sessionOptions));
 
-// Add the following code to enable CORS
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://a6--shiny-queijadas-6fd8c8.netlify.app");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-  
 CourseRoutes(app);
 app.use(express.json());
 UserRoutes(app);
